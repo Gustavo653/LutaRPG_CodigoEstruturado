875dbbaabcd046b5ff13e7cb0a2bc39f474a2c03
Gustavo Henrique da Silva <ujipe@hotmail.com>
2021-04-29 17:50:04 -0300

    Apresentação dos dados e atributos ao usuário

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace LutaRPG_CodigoEstruturado
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bem vindo ao jogo Luta RPG");
+            Console.WriteLine("-------------------------------");//30 hífens
+            Console.WriteLine();
+
+            Console.Write("Insira o nome de seu herói: ");
+            string nomeHeroi = Console.ReadLine();
+            Console.WriteLine("Os pontos de vida de seu herói são: 50 PV");
+            Console.WriteLine("-------------------------------");//30 hífens
+            Console.WriteLine();
+
+            Console.WriteLine("Sua arma corpo a corpo inicial é: Espada Enferrujada");
+            Console.WriteLine("Seus atributos são: ");
+            Console.WriteLine("• Dano: 7 a 23");
+            Console.WriteLine("-------------------------------");//30 hífens
+            Console.WriteLine();
+
+            Console.WriteLine("Seu oponente é o: Troll");
+            Console.WriteLine("Os pontos de vida de seu oponente é de: 30 PV");
+            Console.WriteLine("-------------------------------");//30 hífens
+            Console.WriteLine();
+
+            Console.WriteLine("A arma corpo a corpo de seu oponente é: Machado Corrompido");
+            Console.WriteLine("Os atributos são:");
+            Console.WriteLine("• Dano: 15 a 40");
+            Console.WriteLine("-------------------------------");//30 hífens
+            Console.WriteLine();
+
+            Console.WriteLine("Você deseja lutar contra este oponente? (S / N)");
+            char inicioLuta = char.Parse(Console.ReadLine());
+
+
         }
     }
 }
